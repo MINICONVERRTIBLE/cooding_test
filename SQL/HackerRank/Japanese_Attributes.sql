549c5f724f010f0568f70b707811e6caf0185025
unknown <sojihae99@gmail.com>
2023-01-16 22:44:49 +0900

    230116_sql폴더 추가

---
+++ SQL/HackerRank/Japanese_Attributes.sql
@@ -0,0 +1,9 @@
+/*
+Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+The CITY table is described as follows:
+https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true
+*/
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
---
+++ SQL/HackerRank/Weather_Observation_Station1.sql
@@ -0,0 +1,8 @@
+/*
+Query a list of CITY and STATE from the STATION table.
+The STATION table is described as follows:
+https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
+*/
+
+SELECT CITY, STATE 
+FROM STATION;
