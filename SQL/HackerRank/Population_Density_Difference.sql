161726cb0a842238eb54c155de3f993967f0ec3b
unknown <sojihae99@gmail.com>
2023-01-18 20:10:02 +0900

    Population Density Difference Solved

---
+++ SQL/HackerRank/Population_Density_Difference.sql
@@ -0,0 +1,10 @@
+/*
+Query the difference between the maximum and minimum populations in CITY.
+
+Input Format
+
+The CITY table is described as follows: CITY.jpg
+https://www.hackerrank.com/challenges/population-density-difference/problem*/
+
+SELECT MAX(POPULATION)-MIN(POPULATION)
+FROM City;
